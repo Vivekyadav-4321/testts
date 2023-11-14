72418f3338acf26e6e66496f1c43faaf24f8a887
Vivekyadav-4321 <96228595+Vivekyadav-4321@users.noreply.github.com>
2023-11-14 16:50:43 +0530

    Update server.js

--- dest/server.js
+++ dest/server.js
@@ -6,6 +6,9 @@ const app = express();
 const PORT = 3000 || process.env.PORT;
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
+app.post("/test", (req, res)=>{
+    console.log("hi")
+})
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api/admin/', admin_routes_1.adminRouter);
